7c0d782dec9e8964ef6de6d1fab34a898d7b6605
Jordan Stoddard <jjsincorporated@gmail.com>
2019-07-23 14:13:53 -0700

    implement branch table

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,6 +1,10 @@
 """CPU functionality."""
-
 import sys
+
+PRN = 0b01000111
+LDI = 0b10000010
+HLT = 0b00000001
+MUL = 0b10100010
 
 
 class CPU:
@@ -11,6 +15,38 @@ class CPU:
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
+        self.branchtable = {}
+        self.branchtable[PRN] = self.handle_PRN
+        self.branchtable[LDI] = self.handle_LDI
+        self.branchtable[HLT] = self.handle_HLT
+        self.branchtable[MUL] = self.handle_MUL
+
+    def handle_operations(self, IR, operand_a, operand_b, distance):
+        if IR == LDI:
+            self.branchtable[IR](operand_a, operand_b, distance)
+        elif IR == PRN:
+            self.branchtable[IR](operand_a, distance)
+        elif IR == MUL:
+            self.branchtable[IR](operand_a, operand_b, distance)
+        elif IR == HLT:
+            self.branchtable[IR]()
+
+
+    def handle_MUL(self, operand_a, operand_b, distance):
+        self.alu('MUL', operand_a, operand_b)
+        self.pc += distance
+
+    def handle_HLT(self):
+        running = False
+        return sys.exit(1)
+
+    def handle_PRN(self, operand_a, distance):
+        print(self.reg[operand_a])
+        self.pc += distance
+
+    def handle_LDI(self, operand_a, operand_b, distance):
+        self.reg[operand_a] = operand_b
+        self.pc += distance
 
     def ram_read(self, MAR):
         return self.ram[MAR]
@@ -20,24 +56,6 @@ class CPU:
 
     def load(self):
         """Load a program into memory."""
-
-        # address = 0
-
-        # # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010,  # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111,  # PRN R0
-        #     0b00000000,
-        #     0b00000001,  # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
 
         if len(sys.argv) != 2:
             print(f'usage: {sys.argv[0]} <filename>')
@@ -56,6 +74,7 @@ class CPU:
                     
         except FileNotFoundError:
             print(f'{sys.argv[0]}: {sys.argv[1]} not found.')
+            sys.exit(2)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -90,10 +109,6 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        LDI = 0b10000010
-        PRN = 0b01000111
-        HLT = 0b00000001
-        MUL = 0b10100010
         running = True
         
         while running:
@@ -102,28 +117,4 @@ class CPU:
             operand_b = self.ram_read(self.pc + 2)
             num_operands = (IR & 0b11000000) >> 6
             dist_to_move_pc = num_operands + 1
-            if IR == LDI:
-                self.reg[operand_a] = operand_b
-                self.pc += dist_to_move_pc
-            elif IR == PRN:
-                print(self.reg[operand_a])
-                self.pc += dist_to_move_pc
-            elif IR == MUL:
-                self.alu('MUL', operand_a, operand_b)
-                self.pc += dist_to_move_pc
-            elif IR == HLT:
-                running = False
-                sys.exit(1)
-
-
-"""
-program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-"""
+            self.handle_operations(IR, operand_a, operand_b, dist_to_move_pc)
